184a70a8e19f64e12bea408681e258d37ba0090d
Mohamed Omar <mohamed.omar@brightinsight.com>
2021-05-07 01:48:49 -0700

    Initial configuration

---
+++ src/test/java/io/javabrains/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
